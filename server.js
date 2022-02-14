6e409f93769b0de6191ceb27f8bc5d86282dbcdc
JaredHuang <Hookjover@gmail.com>
2022-02-14 23:35:41 +0800

    change

--- server.js
+++ server.js
@@ -1,4 +1,27 @@
-if (path === "/") {
+var http = require('http')
+var fs = require('fs')
+var url = require('url')
+var port = process.argv[2]
+
+if(!port){
+  console.log('请指定端口号\n 例：node server.js 8888 ')
+  process.exit(1)
+}
+
+var server = http.createServer(function(request, response){
+  var parsedUrl = url.parse(request.url, true)
+  var pathWithQuery = request.url 
+  var queryString = ''
+  if(pathWithQuery.indexOf('?') >= 0){ queryString = pathWithQuery.substring(pathWithQuery.indexOf('?')) }
+  var path = parsedUrl.pathname
+  var query = parsedUrl.query
+  var method = request.method
+
+  /******** 从这里开始看，上面不要看 ************/
+
+  console.log('*有新请求*路径（带查询参数）为：' + pathWithQuery)
+
+  if (path === "/") {
     response.statusCode = 200;
     response.setHeader("Content-Type", "text/html;charset=utf-8");
     response.write(`
@@ -18,3 +41,10 @@ if (path === "/") {
     response.write(`你访问的页面不存在`);
     response.end();
   }
+
+  /******** 代码结束，下面不要看 ************/
+})
+
+server.listen(port)
+console.log('监听 ' + port + ' 成功\n请在浏览器打开 http://localhost:' + port)
+
